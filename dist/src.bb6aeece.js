d99fbe63052e78ee8e866f92de3852cd0192e3dd
hellomac87 <hellomac87@gmail.com>
2018-10-19 10:29:13 +0900

    Feature : html, css MarkUp

--- dist/src.bb6aeece.js
+++ dist/src.bb6aeece.js
@@ -165,7 +165,7 @@ module.exports = reloadCSS;
         module.hot.dispose(reloadCSS);
         module.hot.accept(reloadCSS);
       
-},{"_css_loader":10}],13:[function(require,module,exports) {
+},{"_css_loader":10}],25:[function(require,module,exports) {
 var global = arguments[3];
 var OVERLAY_ID = '__parcel__error__overlay__';
 
@@ -335,4 +335,4 @@ function hmrAccept(bundle, id) {
     return hmrAccept(global.parcelRequire, id);
   });
 }
-},{}]},{},[13], null)
+},{}]},{},[25], null)
